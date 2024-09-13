b67317804ddbf647b6f2c4b0f5bccbd27d6f30d2
sjnosukequy <vuongquy51@gmail.com>
2024-09-13 22:45:06 +0700

    Move tmp folder to root

--- database/sql.jsx
+++ database/sql.jsx
@@ -2,7 +2,7 @@ import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import path from 'path';
 
-const dbPath = path.join(process.cwd(), 'app/tmp/DB.db');
+const dbPath = path.join(process.cwd(), 'tmp/DB.db');
 
 export const db = await open({
     filename: dbPath,
